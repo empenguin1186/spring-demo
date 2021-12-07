105601519f3d91df71e5e3038cd0df0e90515e18
empenguin1186 <emperorpenguint1186@gmail.com>
2021-12-08 00:00:21 +0900

    :+1: add select query

--- src/main/kotlin/com/example/demo/infra/repository/mapper/TaskMapper.kt
+++ src/main/kotlin/com/example/demo/infra/repository/mapper/TaskMapper.kt
@@ -3,6 +3,7 @@ package com.example.demo.infra.repository.mapper
 import com.example.demo.domain.task.Task
 import org.apache.ibatis.annotations.Insert
 import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Select
 import org.springframework.stereotype.Component
 
 @Mapper
@@ -17,4 +18,7 @@ interface TaskMapper {
        )
     """)
     fun insert(task: Task)
+
+    @Select("""SELECT * FROM Tasks WHERE id = #{taskId}""")
+    fun findByTaskId(taskId: Int)
 }
